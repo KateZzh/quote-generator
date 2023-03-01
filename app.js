c3e033cd6ae6afc5d0425cbb006d66529ebd18b1
KateZzh <ekaterin.pro2@gmail.com>
2023-03-01 21:18:53 +0300

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const arr = [
 const dynamicText = document.querySelector(".wrapper p");
 
 function doDynamic(arr_, dynamicText_) {
-  const index = Math.round(Math.random() * arr_.length-1);
+  const index = Math.round(Math.random() * (arr_.length-1));
   dynamicText_.innerHTML = arr_[index];
 }
 
